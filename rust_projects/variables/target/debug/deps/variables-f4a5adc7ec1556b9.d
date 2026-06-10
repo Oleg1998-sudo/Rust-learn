ed7ccd308ba8d13bfe939c49d79116d6d8b1b9a5
Oleg Shatalin <o.shatalin@setupit.org>
2026-06-10 22:05:17 +0300

    variables

---
+++ rust_projects/variables/target/debug/deps/variables-f4a5adc7ec1556b9.d
@@ -0,0 +1,5 @@
+/home/oleg/projects/projects_rust/Rust-learn/rust_projects/variables/target/debug/deps/variables-f4a5adc7ec1556b9.d: src/main.rs
+
+/home/oleg/projects/projects_rust/Rust-learn/rust_projects/variables/target/debug/deps/variables-f4a5adc7ec1556b9: src/main.rs
+
+src/main.rs:
---
+++ rust_projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/oleg/projects/projects_rust/Rust-learn/rust_projects/variables/target/debug/variables: /home/oleg/projects/projects_rust/Rust-learn/rust_projects/variables/src/main.rs
